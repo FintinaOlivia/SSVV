cbad40a09ae37d6e2b758da1d530c40a995e3119
Olivia <olivia.fintina@gmail.com>
2025-03-22 16:01:11 +0200

    Added more comprehensive tests for the addStudent functionality

--- src/main/java/repository/NotaXMLRepository.java
+++ src/main/java/repository/NotaXMLRepository.java
@@ -10,15 +10,6 @@ import validation.ValidationException;
 import validation.Validator;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.*;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
